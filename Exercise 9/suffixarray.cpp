0492475b04ccfc65275a11a7a33aa0508152d208
Andreas Nordström <andno773@student.liu.se>
2025-12-17 14:48:05 +0100

    suffixarray done, very slow

--- Exercise 9/suffixarray.cpp
+++ Exercise 9/suffixarray.cpp
@@ -1,35 +1,112 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cstdio>
-#include <cstring>
+#include <string>
 
-
-
-std::vector<std::string> suffixArray(std::vector<std::string> input)
+std::string reconstruct(int n, const std::vector<std::pair<int,std::string>>& suffixes)
 {
-    return 0;
-    //Should return sorted array of all suffixes of a string
-    //strcmp??
-    //Split string into n-gram?? abba whould be {ab, bb, ba}
-    //lexicographic renaming?
+    std::vector<char> s(n, '?');
+
+    for (auto& p : suffixes)
+    {
+        int pos = p.first - 1;
+        std::string suf = p.second;
+        size_t star = suf.find('*');
+
+        if (star == std::string::npos)
+        {
+            for (size_t i = 0; i < suf.size(); ++i)
+            {
+                int idx = pos + i;
+                if (idx >= n)
+                {
+                    return "IMPOSSIBLE";
+                }
+                if (s[idx] == '?' || s[idx] == suf[i])
+                {
+                    s[idx] = suf[i];
+                }
+                else
+                {
+                    return "IMPOSSIBLE";
+                }
+            }
+        }
+        else
+        {
+            std::string prefix = suf.substr(0, star);
+            std::string suffix = suf.substr(star + 1);
+            int prefixLen = prefix.size();
+            int suffixLen = suffix.size();
+
+            for (int i = 0; i < prefixLen; ++i)
+            {
+                int idx = pos + i;
+                if (idx >= n)
+                {
+                    return "IMPOSSIBLE";
+                }
+                if (s[idx] == '?' || s[idx] == prefix[i])
+                {
+                    s[idx] = prefix[i];
+                }
+                else
+                {
+                    return "IMPOSSIBLE";
+                }
+            }
+
+            for (int i = 0; i < suffixLen; ++i)
+            {
+                int idx = n - suffixLen + i;
+                if (idx < pos + prefixLen || idx >= n)
+                {
+                    return "IMPOSSIBLE";
+                }
+                if (s[idx] == '?' || s[idx] == suffix[i])
+                {
+                    s[idx] = suffix[i];
+                }
+                else
+                {
+                    return "IMPOSSIBLE";
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (s[i] == '?')
+        {
+            s[i] = 'a';
+        }
+    }
+
+    return std::string(s.begin(), s.end());
 }
-
-
 
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    int n;
-    std::cin >> n;
-    
-    for (int i = 0; i < n; ++i)
+    int T;
+    std::cin >> T;
+
+    while (T--)
     {
-        std::cin >> input;
-        std::string trie = suffixTrie(input);
-        std:string result = SuffixArray(trie);
-        std::cout <<  result << '\n';
+        int n, m;
+        std::cin >> n >> m;
+        std::vector<std::pair<int,std::string>> suffixes(m);
+
+        for (int i = 0; i < m; ++i)
+        {
+            std::cin >> suffixes[i].first >> suffixes[i].second;
+        }
+
+        std::string ans = reconstruct(n, suffixes);
+        std::cout << ans << "\n";
     }
+
     return 0;
 }
+
+
+
