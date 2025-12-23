be0d935936b7cdda403f1b56c7efd31b50f4bad6
Aadak9 <your@email.com>
2025-12-23 10:21:27 +0100

    Started trilemma

---
+++ Exercise 12/trilemma.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+struct Point
+{
+    double x;
+    double y;
+};
+
+int trilemma(const Point& p1, const Point& p2, const Point& p3)
+{
+    return case;
+}
+
+int main()
+{
+    int N;
+    std::cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        Point p1, p2, p3;
+        std::cin >> p1.x >> p1.y >> p2.x >> p2.y >> p3.x >> p3.y;
+        int case = trilemma(p1, p2, p3);
+        //TODO: switch or if-statements
+    }
+}
